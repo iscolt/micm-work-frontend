567929218fb696c6bc28c7dbc3c31f5ec3eb9c56
isColt <1329208516@qq.com>
2020-10-12 21:05:15 +0800

    -简单适配手机端_登陆页

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,35 +1,65 @@
 <template>
     <div style="padding: 20px;">
-        <el-page-header @back="$router.push('/')" content=""></el-page-header>
-        <el-row :gutter="20" style="margin-top: 36px">
-            <el-col :span="8"><div class="grid-content bg-purple" style="color: white">.</div></el-col>
-            <el-col :span="8">
-                <el-form ref="form" :model="form" label-width="80px">
-                    <div style="width: 100%;display: flex;justify-content: center;">
-                        <h2>用户登录</h2>
-                    </div>
-                    <br/><br/>
-                    <el-form-item label="学号">
-                        <el-input v-model="form.number"></el-input>
-                    </el-form-item>
-                    <el-form-item label="密码">
-                        <el-input type="password" v-model="form.password"></el-input>
-<!--                        <el-link type="warning" :underline="false" @click="$router.push('/forget')">-->
-<!--                            忘记密码？-->
-<!--                        </el-link>-->
-                    </el-form-item>
-                    <el-form-item>
-                        <el-radio v-model="form.apply" label="1">《第一次输入的密码自动设为登陆密码》</el-radio>
-                        <el-radio v-model="form.apply" label="0">拒绝</el-radio>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="login_pwd">登录</el-button>
-                        <el-button>注册</el-button>
-                    </el-form-item>
-                </el-form>
-            </el-col>
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-        </el-row>
+<!--        <el-page-header @back="$router.push('/')" content=""></el-page-header>-->
+<!--        <el-row :gutter="20" style="margin-top: 36px">-->
+<!--            <el-col :span="8"><div class="grid-content bg-purple" style="color: white">.</div></el-col>-->
+<!--            <el-col :span="8">-->
+<!--                <el-form ref="form" :model="form" label-width="80px">-->
+<!--                    <div style="width: 100%;display: flex;justify-content: center;">-->
+<!--                        <h2>用户登录</h2>-->
+<!--                    </div>-->
+<!--                    <br/><br/>-->
+<!--                    <el-form-item label="学号">-->
+<!--                        <el-input v-model="form.number"></el-input>-->
+<!--                    </el-form-item>-->
+<!--                    <el-form-item label="密码">-->
+<!--                        <el-input type="password" v-model="form.password"></el-input>-->
+<!--&lt;!&ndash;                        <el-link type="warning" :underline="false" @click="$router.push('/forget')">&ndash;&gt;-->
+<!--&lt;!&ndash;                            忘记密码？&ndash;&gt;-->
+<!--&lt;!&ndash;                        </el-link>&ndash;&gt;-->
+<!--                    </el-form-item>-->
+<!--                    <el-form-item>-->
+<!--                        <el-radio v-model="form.apply" label="1">《第一次输入的密码自动设为登陆密码》</el-radio>-->
+<!--                        <el-radio v-model="form.apply" label="0">拒绝</el-radio>-->
+<!--                    </el-form-item>-->
+<!--                    <el-form-item>-->
+<!--                        <el-button type="primary" @click="login_pwd">登录</el-button>-->
+<!--                        <el-button>注册</el-button>-->
+<!--                    </el-form-item>-->
+<!--                </el-form>-->
+<!--            </el-col>-->
+<!--            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>-->
+<!--        </el-row>-->
+      <el-page-header @back="$router.push('/')" content=""></el-page-header>
+      <el-row :gutter="20" style="margin-top: 36px">
+        <el-col :xs="1" :sm="1" :md="1" :lg="8" :xl="8"><div style="color: white" class="grid-content bg-purple">.</div></el-col>
+        <el-col :xs="22" :sm="22" :md="22" :lg="8" :xl="8"><div class="grid-content bg-purple-light">
+          <el-form ref="form" :model="form" :label-width="mobile ? '40px' : '80px'">
+            <div style="width: 100%;display: flex;justify-content: center;">
+              <h2>用户登录</h2>
+            </div>
+            <br/><br/>
+            <el-form-item label="学号">
+              <el-input v-model="form.number"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+              <el-input type="password" v-model="form.password"></el-input>
+              <!--                        <el-link type="warning" :underline="false" @click="$router.push('/forget')">-->
+              <!--                            忘记密码？-->
+              <!--                        </el-link>-->
+            </el-form-item>
+            <el-form-item>
+              <el-radio v-model="form.apply" label="1">第一次输入的为登陆密码</el-radio>
+              <el-radio v-model="form.apply" label="0">拒绝</el-radio>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="login_pwd">登录</el-button>
+              <el-button>注册</el-button>
+            </el-form-item>
+          </el-form>
+        </div></el-col>
+        <el-col :xs="1" :sm="1" :md="1" :lg="8" :xl="8"><div class="grid-content bg-purple-light"></div></el-col>
+      </el-row>
     </div>
 </template>
 
@@ -44,7 +74,8 @@
                     number: '',
                     password: '',
                     apply: '1',
-                }
+                },
+                mobile: this.isMobile()
             }
         },
         created() {
